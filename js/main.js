3b292baa1ba4b28f82f67f97316275f28a8a9128
lroco <l.rocoski@gmail.com>
2020-12-02 14:36:09 -0500

    New repo for website

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  } 
+
+  const navbar = document.querySelector(".navbar");
+  const menu = document.querySelector(".menu-list");
+  const menuBtn = document.querySelector(".menu-btn");
+  const cancelBtn = document.querySelector(".cancel-btn");
+  
+  menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menu.classList.add("hide");
+  }
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menu.classList.remove("hide");
+
+}
+window.onscroll = ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
