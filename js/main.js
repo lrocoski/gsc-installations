7b7483d2b7061c76c761a51d93224999d3ccaa14
lroco <l.rocoski@gmail.com>
2020-12-03 11:43:33 -0500

    Updated home page nav bar javascript

--- js/main.js
+++ js/main.js
@@ -1,43 +1,19 @@
 const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
-  menu.classList.add("active");
-  menuBtn.classList.add("hide");
-  cancelBtn.classList.add("show");
-  body.classList.add("disabledScroll");
-}
-cancelBtn.onclick = ()=>{
-  menu.classList.remove("active");
-  menuBtn.classList.remove("hide");
-  cancelBtn.classList.remove("show");
-  body.classList.remove("disabledScroll");
-}
 
-window.onscroll = ()=>{
+menuBtn.onClick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onClick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onScroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
 
-// function myFunction(x) {
-//     x.classList.toggle("change");
-//   } 
-
-//   const navbar = document.querySelector('.navbar');
-//   const menu = document.querySelector('.menu-list');
-//   const menuBtn = document.querySelector('.menu-btn');
-//   const cancelBtn = document.querySelector('.cancel-btn');
-  
-// menuBtn.onclick = ()=>{
-//       menu.classList.add("active");
-//       menu.classList.add("hide");
-//   }
-// cancelBtn.onclick = ()=>{
-//     menu.classList.remove("active");
-//     menu.classList.remove("hide");
-
-// }
-// window.onscroll = ()=>{
-//     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-// }
