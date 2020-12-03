81b2f90610450ae63f59adc650ae820eb3729961
lroco <l.rocoski@gmail.com>
2020-12-03 08:59:36 -0500

    Updated the javascript and the navbar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  } 
-
-  const navbar = document.querySelector('.navbar');
-  const menu = document.querySelector('.menu-list');
-  const menuBtn = document.querySelector('.menu-btn');
-  const cancelBtn = document.querySelector('.cancel-btn');
-  
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
 menuBtn.onclick = ()=>{
-      menu.classList.add("active");
-      menu.classList.add("hide");
-  }
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+  body.classList.add("disabledScroll");
+}
 cancelBtn.onclick = ()=>{
-    menu.classList.remove("active");
-    menu.classList.remove("hide");
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  body.classList.remove("disabledScroll");
+}
 
-}
 window.onscroll = ()=>{
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+// function myFunction(x) {
+//     x.classList.toggle("change");
+//   } 
+
+//   const navbar = document.querySelector('.navbar');
+//   const menu = document.querySelector('.menu-list');
+//   const menuBtn = document.querySelector('.menu-btn');
+//   const cancelBtn = document.querySelector('.cancel-btn');
+  
+// menuBtn.onclick = ()=>{
+//       menu.classList.add("active");
+//       menu.classList.add("hide");
+//   }
+// cancelBtn.onclick = ()=>{
+//     menu.classList.remove("active");
+//     menu.classList.remove("hide");
+
+// }
+// window.onscroll = ()=>{
+//     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+// }
