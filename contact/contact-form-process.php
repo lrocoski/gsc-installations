4159ffdcb917fd5268c36b6868406e4fbcf164e1
phatcycotic <79103228+phatcycotic@users.noreply.github.com>
2021-02-26 16:28:56 -0500

    Update contact-form-process.php

--- contact/contact-form-process.php
+++ contact/contact-form-process.php
@@ -1,14 +1,14 @@
 <?php
 
 if (isset($_POST['submit'])) {
-  $Fname = $_POST['Fname'];
-  $Lname = $_POST['Lname'];
+  $firstname = $_POST['Fname'];
+  $Lastname = $_POST['Lname'];
   $mailFrom = $_POST['Email'];
   $message = $_POST['subject'];
 
   $mailTo = "Kyle_Moore2@hotmail.com";
   $headers = "From: ".$mailFrom;
-  $txt = "You have received an e-mail from ".$Fname.".\n\n".$message;
+  $txt = "You have received an e-mail from ".$firstname.".\n\n".$message;
 
   mail($mailTo, $subject, $txt, $headers);
   header("Location: contact/index.html");
