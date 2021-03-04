a953d6493f1c4c1d8525e0ea88ca772982b4d59c
phatcycotic <79103228+phatcycotic@users.noreply.github.com>
2021-03-04 00:00:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
 menuBtn.onclick = ()=>{
     navbar.classList.add("show");
     menuBtn.classList.add("hide");
